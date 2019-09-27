e930c7ba3c9d0e48338c7f5de1dd9b390a47af1e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-26 23:01:52 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,21 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['q'] = params[:query]
+        req.params['sort'] = 'updated'
+      end
+      body_hash = JSON.parse(@resp.body)
+      if @resp.success?
+        @repos = body_hash["items"]
+      else
+        @error = "Please enter a valid query"
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search'
   end
 end
